6469bd7bb5d0e755f745611a303430ac388cd8f9
Tanya Fedko <Tanya Fedko>
2017-03-22 15:00:34 +0100

    First commit

---
+++ src/main/java/PadawanBlog/Application.java
@@ -0,0 +1,20 @@
+package PadawanBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tanya on 22.03.2017.
+ */
+
+    @SpringBootApplication
+    public class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+
+        }
+    }
+
+
+
